e7b2a8212d6edc6c1c6d188edea21159cf7a29e4
Ana Claudia Vignola <anacvignola@riseup.net>
2018-08-06 18:03:23 -0300

    Problem 1072 - Interval 2

---
+++ 1072.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+  
+int main(){
+	int n, i;
+	long int num;
+	long int in = 0, out = 0;
+	cin >> n;
+	for (i = 0; i < n; i++){
+		cin >> num;
+		if(num >= 10 && num <= 20)
+			in++;
+		else
+			out++;
+	}
+    cout << in << " in" << endl << out << " out" << endl;
+	return 0;
+}
